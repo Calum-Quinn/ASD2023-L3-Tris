7441510812b0401951262a96d9d3ac6eaded19de
Olivier Cuisenaire <olivier.cuisenaire@heig-vd.ch>
2021-03-22 16:02:55 +0100

    Enoncé labo 3

---
+++ src/tri_rapide.h
@@ -0,0 +1,20 @@
+#ifndef TRI_RAPIDE
+#define TRI_RAPIDE
+
+template<typename Iterator>
+Iterator partition(Iterator first, Iterator last)
+{
+
+    // a compléter
+
+}
+
+template<typename Iterator>
+void tri_rapide(Iterator first, Iterator last)
+{
+
+    // a compléter
+
+}
+
+#endif
