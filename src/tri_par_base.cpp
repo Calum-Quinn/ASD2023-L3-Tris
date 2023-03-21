09afc815fcaa522f63b4ed4fd262ce61797c4e60
calum <calum.quinn@heig-vd.ch>
2023-03-21 14:46:49 +0100

    Rien fait

--- src/tri_par_base.cpp
+++ src/tri_par_base.cpp
@@ -1,5 +1,5 @@
 /* -----------------------------------------------------------------------------------
-Nom du fichier : tri_par_base.h (xxx = h ou cpp)
+Nom du fichier : tri_par_base.cpp (xxx = h ou cpp)
 Auteur(s)      : Ewan Mariaux, Calum Quinn, Dario Vasques
 Date creation  : 20.03.2023
 Description    : Programme qui implémente le tri par base
@@ -8,17 +8,24 @@ Compilateurs   : Apple clang version 14.0.0 (clang-1400.0.29.102) (Dario)
                : Mingw-w64 g++ 12.2.0 (Calum et Ewan)
 ----------------------------------------------------------------------------------- */
 
-#ifndef TRI_PAR_BASE
-#define TRI_PAR_BASE
+#include "tri_par_base.h"
 
-#include <iterator>
-#include <type_traits>
-#include "some_bits.h"
+using namespace std;
 
 template<typename Iterator, typename Fn>
-void tri_comptage(Iterator first, Iterator last, Iterator output_first, Fn index_fn, size_t N);
+void tri_comptage(Iterator first, Iterator last, Iterator output_first, Fn index_fn, size_t N)
+{
+
+    // a compléter
+
+}
 
 template<typename Iterator, size_t NBITS>
-void tri_par_base(Iterator first, Iterator last);
+void tri_par_base(Iterator first, Iterator last)
+{
+    using T = typename Iterator::value_type;
+    static_assert(is_unsigned<T>::value);
 
-#endif
+    // a compléter
+
+}
