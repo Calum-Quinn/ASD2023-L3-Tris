d729c9e821d459907f7d8031daf630291edf3db0
calum <calum.quinn@heig-vd.ch>
2023-03-21 16:36:22 +0100

    Premiers pas

--- src/tri_par_base.cpp
+++ src/tri_par_base.cpp
@@ -13,19 +13,46 @@ Compilateurs   : Apple clang version 14.0.0 (clang-1400.0.29.102) (Dario)
 using namespace std;
 
 template<typename Iterator, typename Fn>
-void tri_comptage(Iterator first, Iterator last, Iterator output_first, Fn index_fn, size_t N)
-{
+void tri_comptage(Iterator first, Iterator last, Iterator output_first, Fn index_fn, size_t N) {
+    //Pseudo code
+//    fonction TriComptage(A,n,b,clé):
+//    C ← tableau de b compteurs à zéro
+    vector<int> c(N, 0);
+//    pour tout e dans A
+    for(Iterator i = first; i < last; ++i){
+        //    C[clé(e)] += 1
+        c[cle(*i)] += 1;
+    }
 
-    // a compléter
-
+//    idx ← 1
+//    pour i de 1 à b
+//    tmp ← C[i]
+//    C[i] ← idx
+//    idx += tmp
+//    B = tableau de même taille que A
+//    pour tout e dans A
+//    B[C[clé(e)]] ← déplacer e
+//    C[clé(e)] += 1
+//    return B
 }
 
 template<typename Iterator, size_t NBITS>
-void tri_par_base(Iterator first, Iterator last)
-{
+void tri_par_base(Iterator first, Iterator last) {
+
     using T = typename Iterator::value_type;
     static_assert(is_unsigned<T>::value);
 
-    // a compléter
+
+    unsigned max = *max_element(first,last);
+
+    //Pseudo code
+//    fonction triParBase(T, d):
+//    Pour i allant de d à 1
+//for (int i = d; i > 0; --i) {
+//
+//}
+//    Trier avec un tri
+//    stable le tableau T
+//    selon le i-ème chiffre
 
 }
--- src/tri_rapide.cpp
+++ src/tri_rapide.cpp
@@ -13,17 +13,109 @@ Compilateurs   : Apple clang version 14.0.0 (clang-1400.0.29.102) (Dario)
 using namespace std;
 
 template<typename Iterator>
-Iterator partition(Iterator first, Iterator last)
-{
+Iterator partition(Iterator first, Iterator last) {
 
-    // a compléter
 
+
+//    template <typename T>
+//    int partition (T tableau[], int debut, int fin) {
+//
+//        int p = fin;
+//        int i = debut;
+//        int j = p-1;
+//
+//        while(true) {
+//            // avancer i
+//            while (tableau[i] < tableau[p])
+//                ++i;
+//
+//            // reculer j
+//            while (debut <= j and tableau[j] > tableau[p])
+//                --j;
+//
+//            // indices rejoints ou croisés
+//            if (i >= j)
+//                break;
+//
+//            // échanger les valeurs
+//            swap(tableau[i], tableau[j]);
+//        }
+//
+//        // mettre la valeur de pivot à sa place
+//        swap(tableau[i], tableau[p]);
+//
+//        return i;
+//    }
 }
 
 template<typename Iterator>
-void tri_rapide(Iterator first, Iterator last)
-{
+void tri_rapide(Iterator first, Iterator last) {
 
-    // a compléter
+    Iterator p; //pivot
 
+    while(last - first > 0){
+
+      //  p = nbreAleaoire;
+
+      swap(*last, *p);
+
+    }
+
+
+
+
+
+
+
+
+
+
+//    fonction TriRapide(A,lo,hi)
+//    tant que lo < hi
+//    p ← choisir l’élément pivot
+//    permuter A(hi) et A(p)
+//    i ← Partition(A,lo,hi)
+//    si i-lo < hi-i, alors
+//    TriRapide(A,lo,i-1)
+//    lo ← i+1
+//    sinon,
+//            TriRapide(A,i+1,hi)
+//    hi ← i-1
+//    fin si
+//    fin tant que
+
+
+
+
+//    template <typename T>
+//    void tri_rapide (T tableau[], int taille, bool trace) {
+//        tri_rapide_rec(tableau, taille, 0, taille-1, trace);
+//    }
+//
+//---------------------------------------------------------
+//    template <typename T>
+//    void tri_rapide_rec(T tableau[], int taille, int debut, int fin, bool trace) {
+//        if(debut < fin) {
+//            // pivot systématiquement en position hi
+//            int pivot = partition(tableau, debut, fin);
+//
+//            // affiche les résultats intermédiaires de la partition
+//            if (trace) {
+//                cout << debut << " " << pivot << " " << fin << "    ";
+//                display_tab(tableau, taille);
+//                cout << endl;
+//            }
+//
+//            // appels récursifs
+//            tri_rapide_rec(tableau, taille, debut,   pivot-1, trace);
+//            tri_rapide_rec(tableau, taille, pivot+1, fin    , trace);
+//        }
+//
+//        // cas simples
+//        if (debut == fin and trace) {
+//            cout << debut << " " << debut << " " << fin << "    ";
+//            display_tab(tableau, taille);
+//            cout << endl;
+//        }
+//    }
 }
