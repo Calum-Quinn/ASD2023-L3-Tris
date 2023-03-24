3b8958ff1856c8101e0333df0f8950c50a56c7eb
dariovasq <dario.vasques@heig-vd.ch>
2023-03-24 21:11:07 +0100

    program 2

--- src/program_2.cpp
+++ src/program_2.cpp
@@ -9,23 +9,31 @@
 using namespace std;
 
 int main() {
-   //vector<unsigned> vec = {93, 45, 23, 143, 34, 0, 110, 67, 54, 76, 174};
-   vector<unsigned> vec = {93, 45, 23, 140, 234, 32, 1};
+   vector<unsigned> vec = {93, 45, 23, 143, 34, 0, 110, 67, 54, 76, 174};
+   vector<int> vec1 = {93, 45, 23, 143, 34, 0, 110};
 
-   for(int i : vec){
+   for(int i : vec1){
       cout << i << " ";
    }
 
    cout << endl;
 
-   cout << mesure_temps(vec, tri_rapide<vector<unsigned>::iterator>);
-   //tri_rapide<vector<unsigned>::iterator>(vec.begin(), vec.end() - 1);
+   //cout << mesure_temps(vec1, tri_rapide<vector<unsigned>::iterator>);
+   tri_rapide<vector<int>::iterator>(vec1.begin(), vec1.end() - 1);
 
    cout << endl;
 
-   for(int i : vec){
+   for(int i : vec1){
       cout << i << " ";
    }
+
+   cout << endl << endl;
+
+   cout << "######################" << endl;
+   cout << "#####TRI PAR BASE#####" << endl;
+   cout << "######################" << endl;
+
+   cout << mesure_temps(vec, tri_par_base<vector<unsigned>::iterator, 3>);
 
    return 0;
 }
