cf062fdd0521bacc7425230c27762a58764b9f40
dariovasq <dario.vasques@heig-vd.ch>
2023-03-31 20:41:24 +0200

    correction du bug

--- src/program_1.cpp
+++ src/program_1.cpp
@@ -18,63 +18,67 @@ using namespace std;
 
 int main() {
 
-    const unsigned seed = 67;
+	const unsigned SEED = 67;
+	const size_t NB_TRIS = 4;
+	const size_t TAILLE_VEC = 100;
 
-    //Génération des vecteurs à triés avec différents remplissages
-    vector<vector<unsigned>> vecteurs = {
-            generateVector<unsigned>(100, seed, typeTri::CROISSANT),
-            generateVector<unsigned>(100, seed, typeTri::DECROISSANT),
-            generateVector<unsigned>(100, seed, typeTri::ALEATOIRE),
-            generateVector<unsigned>(100, seed, typeTri::PRESQUETRIE)
-    };
+	vector<vector<unsigned>> vecteurs(NB_TRIS);
+
+	//Génération des vecteurs à triés avec différents remplissages
+	for(size_t i = 0; i < vecteurs.size(); ++i){
+		vecteurs.at(i) = generateVector<unsigned>(TAILLE_VEC, SEED, typeTri(i));
+	}
+
+	cout << "######################" << endl;
+	cout << "######TRI RAPIDE######" << endl;
+	cout << "######################" << endl;
+
+	cout << endl;
+
+	//Test partition
+	for(vector<unsigned> vec : vecteurs){
+		auto i = partition<vector<unsigned>::iterator>(vec.begin(), vec.end());
+
+		//Contrôle que les valeurs avant le pivot soit plus petit
+		bool controle = true;
+		for (auto j = vec.begin(); j != i; ++j) {
+			if (*j > *i)
+				controle = false;
+		}
+
+		//Contrôle que les valeurs après le pivot soit plus grand
+		for (auto k = vec.end(); k != i; --k) {
+			if (*k < *i)
+				controle = false;
+		}
+
+		cout << boolalpha << "Partition OK ? : " << controle << endl;
+	}
+
+	cout << endl;
+
+	//Test Tri rapide
+	for(vector<unsigned> vec : vecteurs){
+		tri_rapide<vector<unsigned>::iterator>(vec.begin(), vec.end() - 1);
+
+		cout << boolalpha << "Est trie ? "
+			  << is_sorted(vec.begin(), vec.end()) << endl;
+	}
+
+	cout << endl << endl;
 
 
-    cout << "######################" << endl;
-    cout << "######TRI RAPIDE######" << endl;
-    cout << "######################" << endl;
+	cout << "######################" << endl;
+	cout << "#####TRI PAR BASE#####" << endl;
+	cout << "######################" << endl;
 
-   cout << endl;
+	cout << endl;
 
-   //Test partition
-   for(vector<unsigned> vec : vecteurs){
-       vector<unsigned>::iterator i = partition<vector<unsigned>::iterator>(vec.begin(), vec.end());
+	//Test tri par base qui comprend un test du tri comptage
+	for (vector<unsigned> vec : vecteurs) {
+		tri_par_base<vector<unsigned>::iterator,3>(vec.begin(),vec.end() - 1);
+		cout << boolalpha << "Est trie ? " << is_sorted(vec.begin(), vec.end()) << endl;
+	}
 
-       //Contrôle que les valeurs avant le pivot soit plus petit
-       bool controle = true;
-       for (vector<unsigned>::iterator j = vec.begin(); j != i; ++j) {
-           if (*j > *i)
-               controle = false;
-       }
-       //Contrôle que les valeurs après le pivot soit plus grand
-       for (vector<unsigned>::iterator j = vec.end(); j != i; --j) {
-           if (*j < *i)
-               controle = false;
-       }
-
-       cout << boolalpha << "Partition OK ? : " << controle << endl;
-   }
-
-    //Test Tri Rapide
-    for(vector<unsigned> vec : vecteurs){
-        tri_rapide<vector<unsigned>::iterator>(vec.begin(), vec.end() - 1);
-
-        cout << boolalpha << "Est trie ? "
-             << is_sorted(vec.begin(), vec.end()) << endl;
-    }
-
-   cout << endl << endl;
-
-   cout << "######################" << endl;
-   cout << "#####TRI PAR BASE#####" << endl;
-   cout << "######################" << endl;
-
-   cout << endl;
-
-   //Test tri par base qui comprend un test du tri comptage
-   for (vector<unsigned> vec : vecteurs) {
-       tri_par_base<vector<unsigned>::iterator,3>(vec.begin(),vec.end() - 1);
-       cout << boolalpha << "Est trie ? " << is_sorted(vec.begin(), vec.end()) << endl;
-   }
-
-   return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
